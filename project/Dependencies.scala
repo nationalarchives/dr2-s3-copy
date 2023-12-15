8ac4507ec905b7ecdfe21b34e9917c415230ea95
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-12-15 12:55:54 +0000

    Update log4j-core, ... to 2.22.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 import sbt._
 object Dependencies {
-  lazy val logbackVersion = "2.20.0"
+  lazy val logbackVersion = "2.22.0"
   lazy val log4jSlf4j = "org.apache.logging.log4j" % "log4j-slf4j-impl" % logbackVersion
   lazy val log4jCore = "org.apache.logging.log4j" % "log4j-core" % logbackVersion
   lazy val log4jTemplateJson = "org.apache.logging.log4j" % "log4j-layout-template-json" % logbackVersion
@@ -12,6 +12,5 @@ object Dependencies {
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.4"
   lazy val pureConfigCats = "com.github.pureconfig" %% "pureconfig-cats-effect" % "0.17.4"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.12"
-  lazy val reactorTest = "io.projectreactor" % "reactor-test" % "3.5.4"
-  ,
+  lazy val reactorTest = "io.projectreactor" % "reactor-test" % "3.5.4",
 }
