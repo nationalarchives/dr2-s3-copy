f3c77ceeebb8661e06ab263560c49db4d939b55d
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-12-16 08:21:40 +0000

    Update ssm to 2.22.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   lazy val lambdaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
   lazy val s3Client = "uk.gov.nationalarchives" %% "da-s3-client" % "0.1.34"
-  lazy val ssm = "software.amazon.awssdk" % "ssm" % "2.21.46"
+  lazy val ssm = "software.amazon.awssdk" % "ssm" % "2.22.0"
   lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.6"
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.4"
   lazy val pureConfigCats = "com.github.pureconfig" %% "pureconfig-cats-effect" % "0.17.4"
