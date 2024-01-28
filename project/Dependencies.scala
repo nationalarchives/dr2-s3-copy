8677b6dec6fac3c2591033992e74c029f11430a8
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-01-28 08:22:12 +0000

    Update scalatest to 3.2.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,10 +6,10 @@ object Dependencies {
   lazy val log4jSlf4j = "org.apache.logging.log4j" % "log4j-slf4j-impl" % logbackVersion
   lazy val log4jCore = "org.apache.logging.log4j" % "log4j-core" % logbackVersion
   lazy val log4jTemplateJson = "org.apache.logging.log4j" % "log4j-layout-template-json" % logbackVersion
-  lazy val log4CatsCore= "org.typelevel" %% "log4cats-core" % log4CatsVersion;
+  lazy val log4CatsCore = "org.typelevel" %% "log4cats-core" % log4CatsVersion;
   lazy val log4CatsSlf4j = "org.typelevel" %% "log4cats-slf4j" % log4CatsVersion
   lazy val lambdaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.2"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
   lazy val s3Client = "uk.gov.nationalarchives" %% "da-s3-client" % "0.1.34"
   lazy val ssm = "software.amazon.awssdk" % "ssm" % "2.20.68"
   lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.5"
