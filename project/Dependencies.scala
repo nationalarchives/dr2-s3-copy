c73169e0f2acb257247f4d938c7e96cd32e795fa
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-01-31 08:23:34 +0000

    Update reactor-test to 3.6.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,5 +16,5 @@ object Dependencies {
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.5"
   lazy val pureConfigCats = "com.github.pureconfig" %% "pureconfig-cats-effect" % "0.17.5"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.30"
-  lazy val reactorTest = "io.projectreactor" % "reactor-test" % "3.5.14"
+  lazy val reactorTest = "io.projectreactor" % "reactor-test" % "3.6.2"
 }
